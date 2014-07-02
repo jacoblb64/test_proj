ea769fc5094f271aae5b899f3afa511cb3a034a3
Jacob Barnett <jacoblb64@gmail.com>
2014-07-02 10:21:49 -0400

    Minor content changes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
 
+	describe "GET home" do
+		it "should be successful" do
+			get 'home'
+			response.should be_success
+		end
+	end
+
+
 end
